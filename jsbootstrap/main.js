7ddf28879b8aa498c723e913e99742b62bbb0990
mirafzal1604 <mirvosiqovm@gmail.com>
2022-01-29 00:57:02 +0500

    second

--- jsbootstrap/main.js
+++ jsbootstrap/main.js
@@ -1,17 +1,22 @@
 let input = document.querySelector("#input");
+let dog = document.querySelector('#dog');
+let cat = document.querySelector('#cat');
+let horse = document.querySelector('#horse');
+let rabbit = document.querySelector('#rabbit');
 
 input.addEventListener("input", p => {
     var age = p.target.value;
-    document.querySelector('#dog').innerHTML = age * 7 + ' Years Old';
-    document.querySelector('#cat').innerHTML = age * 5.2 + ' Years Old';
-    document.querySelector('#horse').innerHTML = age * 6 + ' Years Old';
-    document.querySelector('#rabbit').innerHTML = age * 7 + ' Years Old';
+
+    dog.innerHTML = Number(age * 7).toFixed(2) + ' Years Old';
+    cat.innerHTML = Number(age * 5.2).toFixed(2) + ' Years Old';
+    horse.innerHTML = Number(age * 6).toFixed(2) + ' Years Old';
+    rabbit.innerHTML = Number(age * 7).toFixed(2) + ' Years Old';
 });
 
 function clr() {
-    document.getElementById('input').value = '';
-    document.getElementById('cat').innerHTML = null;
-    document.getElementById('horse').innerHTML = null;
-    document.getElementById('rabbit').innerHTML = null;
-    document.getElementById('dog').innerHTML = null;
+    input.value = '';
+    dog = null;
+    cat = null;
+    horse = null;
+    rabbit = null;
 }
